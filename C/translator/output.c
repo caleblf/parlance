c6e8796c0d6214705871707cd3442360759a7d7b
Chris Phifer <cphifer97@gmail.com>
2016-10-15 09:59:46 -0400

    Added FizzBuzz to website

---
+++ C/translator/output.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+int main() {
+int i;
+for ( i =  1 ;  i <  101 ;  ++ i) {
+if ( i %  3  ==  0  && i %  5  ==  0 ) {
+ printf(  "FizzBuzz" );
+
+}
+
+else if ( i %  3  ==  0 ) {
+ printf(  "Fizz" );
+
+}
+
+else if ( i %  5  ==  0 ) {
+ printf(  "Buzz" );
+
+}
+
+else {
+ printf(  "%d" ,  i);
+
+}
+
+ printf(  "\n" );
+
+}
+
+
+}
+
+
