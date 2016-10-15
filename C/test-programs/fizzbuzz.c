490fc04c14e3c224027d2f6ad54af3273c9af7fc
Chris Phifer <cphifer97@gmail.com>
2016-10-14 21:01:27 -0400

    Made code better

--- C/test-programs/fizzbuzz.c
+++ C/test-programs/fizzbuzz.c
@@ -4,14 +4,16 @@ int main()
 {
         for (int i = 1; i <= 100; ++i) {
                 if (i % 3 == 0 && i % 5 == 0) {
-                        printf("FizzBuzz\n");
+                        printf("FizzBuzz");
                 } else if (i % 3 == 0) {
-                        printf("Fizz\n");
+                        printf("Fizz");
                 } else if (i % 5 == 0) {
-                        printf("Buzz\n");
+                        printf("Buzz");
                 } else {
-                        printf("%d\n", i);
+                        printf("%d", i);
                 }
+
+                printf("\n");
         }
         return 0;
 }
