292625b06d147c812e5d639050ca6d358e277eab
Chris Phifer <cphifer97@gmail.com>
2016-10-15 02:23:21 -0400

    Wrote some translation code

---
+++ C/translator/translator.py
@@ -0,0 +1,38 @@
+# Parlance to C Translator
+# By: Harrison Kaiser
+#     Caleb Lucas-Foley
+#     Chris Phifer
+#
+# This Python script translates a program written
+# in the Parlance language into the C language.
+#
+# Usage: python translator.py <filename>
+
+from sys import argv, exit
+from os import path
+
+def call(words):
+    """Builds a C function call"""
+    output = words.pop(0) + "("
+    word = words.pop(0)
+    args = []
+    while True:
+        if word == "ARG":
+            args.append(expression(words))
+        elif word == "END":
+            return output + ",".join(args) + ")"
+
+        else:
+            exit(1)
+        word = words.pop(0)
+    
+            
+            
+if len(argv) < 2:
+    exit("Usage: " + argv[0] + " <filename>")
+
+if not path.exists(argv[1]):
+    exit("Error: File " + argv[1] + " not found.")
+
+parlance_code = open(argv[1], 'r')
+code_words = parlance_code.read().split()
