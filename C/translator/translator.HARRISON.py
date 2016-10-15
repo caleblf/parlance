fedf5bcfa17a7b71ee76ad5ce4ae82477673a547
uwaces <uwaces@gmail.com>
2016-10-15 04:55:51 -0400

    Added some funcs to

---
+++ C/translator/translator.HARRISON.py
@@ -0,0 +1,105 @@
+# Parlance to C Translator
+# By: Harrison Kaiser
+#     Caleb Lucas-Foley
+#     Chris Phifer
+#
+# This Python script translates a program written
+# in the Parlance language into the C language.
+#
+# Usage: python translator.py <filename>
+
+from sys import argv, exit
+from os import path
+
+def build_call():
+    pass
+
+def build_variable():
+    pass
+
+def build_return():
+    pass
+
+def build_cpp():
+    pass
+
+def build_if():
+    pass
+
+def build_else():
+    pass
+
+def build_loop():
+    pass
+
+def build_end():
+    pass
+
+def build_declaration():
+    pass
+
+statements = {
+    "CALL": build_call,
+    "VARIABLE": build_variable,
+    "RETURN": build_return,
+    "CPP": build_cpp,
+    "IF": build_if,
+    "ELSE": build_else,
+    "LOOP": build_loop,
+    "END": build_end, # Probably don't need this
+    "DECLARE": build_declaration,
+}
+
+def build_call(words):
+    """Builds a C function call"""
+    output = words.pop(0) + "("
+    word = words.pop(0)
+    args = []
+    while True:
+        if word == "ARG":
+            args.append(expression(words))
+        elif word == "END":
+            return output + ",".join(args) + ")"
+
+        else:
+            exit(1)
+        word = words.pop(0)
+    
+def scope(words):
+    output = ""
+    word = words.pop(0)
+            
+if len(argv) < 2:
+    exit("Usage: " + argv[0] + " <filename>")
+
+if not path.exists(argv[1]):
+    exit("Error: File " + argv[1] + " not found.")
+
+parlance_code = open(argv[1], 'r')
+code_words = parlance_code.read().split()
+
+
+
+
+def build_variable(words):
+    return words.pop(0)
+
+def build_return(words):
+    return "return " + build_expression(words) + ";"
+
+def build_cpp(words):
+    output = "#"
+    command = words.pop(0)
+    if (command == "DEFINE") {
+        output += "define " + words.pop(0)
+    } elif (command == "INCLUDES") {
+        output += "include " + words.pop(0)
+    } elif (comman == "IFNDEF") {
+        output += "ifndef " + words.pop(0)
+    } elif (comman == "ENDIF") {
+        output += "ifndef"
+    }
+    return output
+
+def build_if():
+    
